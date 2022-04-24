5ed22a15fdc06895043bdb4995978b8f7f872a7c
Makhmoodjan <meexample@gmail.com>
2022-04-24 16:12:14 +0300

    improving models.py changes 1

--- articles/models.py
+++ articles/models.py
@@ -2,5 +2,8 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
+    timestamp = models.DateTimeField( auto_now_add=True )
+    updated = models.DateTimeField( auto_now=True )
+    publish = models.DateField( auto_now_add=False, auto_now=False, blank=True, null=True )
---
+++ config/tests.py
@@ -0,0 +1,13 @@
+import os
+from django.test import TestCase
+from django.contrib.auth.password_validation import validate_password
+ 
+class TryDjangoConfigTest(TestCase):
+    def test_secret_key_strength(self):
+        DJANGO_SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') 
+        try:
+            is_strong = validate_password(DJANGO_SECRET_KEY)
+        except Exception as e:
+            msg = f" Weak password ->  {e.messages} "
+            self.fail(e)
+
