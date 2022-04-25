41dca78c2181ad824af45a040df2b6247e81df37
Makhmoodjan <meexample@gmail.com>
2022-04-25 16:37:52 +0300

    nothing special

--- config/tests.py
+++ config/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.contrib.auth.password_validation import validate_password
  
 class TryDjangoConfigTest(TestCase):
+    """ Tests for config """
     def test_secret_key_strength(self):
         DJANGO_SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') 
         try:
